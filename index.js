3a52bc15655b0c60cfc07256e852c7479085888e
RaviSir26 <ravikumarsondhiya260498@gmail.com>
2025-08-12 23:36:54 +0530

    first commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT||4000
+const port = process.env.PORT||3001;
 
 server.use(middlewares)
 server.use(router)
 server.listen(port, () => {
   console.log(`JSON Server is running ${port}`)
-})
+});
